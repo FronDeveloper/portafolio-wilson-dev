e7dbb266d67352c5156757b2a2e9b5472d394176
FronDeveloper <wilson.valencia20.wv@gmail.com>
2026-05-09 00:04:03 -0500

    feat: agregar sitemap.xml y robots.txt para SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,14 @@
+// next-sitemap.config.js
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'hhttps://portafolio-wilson-dev.vercel.app', 
-  generateRobotsTxt: true, // Esto crea el archivo robots.txt automáticamente
+  siteUrl: 'https://portafolio-wilson-dev.vercel.app',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
 }
